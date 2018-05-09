a746a20ec59713f7d9c5e3c1d2a96affe7f7ec2f
zhouxinyu <zhouxinyu1216@gmail.com>
2018-05-09 13:21:27 +0900

    Update files

--- cv_test.py
+++ cv_test.py
@@ -1,12 +1,13 @@
 import cv2
 import numpy
 
-img = numpy.zeros((100,100,3),dtype='uint16')
+img = numpy.zeros((100,100,3),dtype='uint8')
 
-img[:,:,0] = numpy.ones((100,100))*64/255.0
+img[:,:,0] = numpy.ones((100,100))*255
 # img[:,:,1] = numpy.ones((10,10))*128/255.0
 # img[:,:,2] = numpy.ones((10,10))*192/255.0
 
 # cv2.imwrite('color_img.jpg', img)
-cv2.imshow("image", img);
+# img = cv2.imread("images/test_rgb_3.png")
+cv2.imshow("images/test_rgb_3.png", img);
 cv2.waitKey();
